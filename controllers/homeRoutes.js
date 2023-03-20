dcd5371c13e9d97a4a657ab7a12500139930fa1d
Chenghao Hou <houchenghao@hotmail.com>
2023-03-20 21:32:02 +1100

    add post update&delete page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,7 +33,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
     try {
       // Find the logged in user based on the session ID
       const postData = await Post.findAll({
-        attributes:['title','body','created_at','userid'],
+        attributes:['id','title','body','created_at','userid'],
         where: [{userid: req.session.user_id}],
         include: [
             { 
@@ -54,21 +54,48 @@ router.get('/dashboard', withAuth, async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
-  });
+});
 
 
-  router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/dashboard');
-      return;
+router.get('/login', (req, res) => {
+// If the user is already logged in, redirect the request to another route
+if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+}
+res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+// If the user is already logged in, redirect the request to another route
+res.render('signup');
+});
+
+router.get('/newpost', (req,res) =>{
+    res.render('newpost');
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        attributes: ['title','body','created_at']
+        // include: [
+        //   {
+        //     model: User,
+        //     attributes: ['name'],
+        //   },
+        // ],
+      });
+  
+      const post = postData.get({ plain: true });
+  
+      res.render('post', {
+        ...post,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
     }
-    res.render('login');
-  });
-
-  router.get('/signup', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    res.render('signup');
   });
 
 // router.get('/post/:id', async (req, res) => {
